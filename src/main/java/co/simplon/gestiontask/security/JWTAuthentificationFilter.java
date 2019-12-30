b6ef80573e6a43b02e46ad5b197100ab9bb241de
laude <timothe.laude@gmail.com>
2019-12-30 05:12:25 +0100

    token jwt

---
+++ src/main/java/co/simplon/gestiontask/security/JWTAuthentificationFilter.java
@@ -0,0 +1,63 @@
+package co.simplon.gestiontask.security;
+
+import co.simplon.gestiontask.entities.AppUser;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class JWTAuthentificationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+    public  JWTAuthentificationFilter(AuthenticationManager authenticationManager){
+        super();
+        this.authenticationManager = authenticationManager;
+    }
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+        throws AuthenticationException{
+        AppUser appUser=null;
+
+        try {
+            appUser = new ObjectMapper().readValue(request.getInputStream(),AppUser.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("****************************");
+        System.out.println("username: "+appUser.getUsername());
+        System.out.println("password: "+appUser.getPassword());
+        return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                appUser.getUsername(),appUser.getPassword()
+        ));
+    }
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain
+                                            ,Authentication authResult)
+        throws IOException, ServletException {
+        User springUser =(User) authResult.getPrincipal();
+        String jwt= Jwts.builder()
+                .setSubject(springUser.getUsername())
+                .setExpiration(new Date(System.currentTimeMillis()+SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256,SecurityConstants.SECRET)
+                .claim("roles",springUser.getAuthorities())
+                .compact();
+        response.addHeader(SecurityConstants.HEADER_STRING,SecurityConstants.TOKEN_PREFIX+jwt);
+        super.successfulAuthentication(request,response,chain,authResult);
+
+    }
+
+}
--- src/main/java/co/simplon/gestiontask/security/SecurityConfig.java
+++ src/main/java/co/simplon/gestiontask/security/SecurityConfig.java
@@ -3,12 +3,15 @@ package co.simplon.gestiontask.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -32,9 +35,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // TODO Auto-generated method stub
         http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.formLogin();//.loginPage("/login");
         http.authorizeRequests().antMatchers("/login/**","/register/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.POST,"/tasks/**").hasAuthority("ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(new JWTAuthentificationFilter(authenticationManager()));
+        http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
     }
