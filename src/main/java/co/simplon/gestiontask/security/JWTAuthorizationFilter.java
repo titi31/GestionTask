2ccc88c13da3205239c50412856b12f6d0022a08
Mr.Robot <classic.graal095@gmail.com>
2020-03-06 18:47:06 +0100

    indentation

--- src/main/java/co/simplon/gestiontask/security/JWTAuthorizationFilter.java
+++ src/main/java/co/simplon/gestiontask/security/JWTAuthorizationFilter.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,44 +16,61 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
-
-public class JWTAuthorizationFilter extends OncePerRequestFilter {
+public class JWTAuthorizationFilter extends OncePerRequestFilter 
+{
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                 FilterChain filterChain)
-            throws ServletException, IOException {
+                                 FilterChain filterChain) throws ServletException, IOException 
+    {
+    
         response.addHeader("Access-Control-Allow-Origin", "*");
+        
         response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, " +
                 "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization");
-        //  response.setHeader("Access-Control-Allow-Methods","*");
-        if (request.getMethod().equals("OPTIONS")) {
+                
+        response.addHeader("Access-Control-Expose-Headers",
+        "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization");
+        
+        if (request.getMethod().equals("OPTIONS")) 
+        {
             response.setStatus(HttpServletResponse.SC_OK);
             System.out.println("options");
-        } else {
+            
+        } 
+        else 
+        {
+        
             System.out.println("else");
             String jwt = request.getHeader(SecurityConstants.HEADER_STRING);
 
             System.out.println(jwt);
-            if (jwt == null || !jwt.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+            
+            if (jwt == null || !jwt.startsWith(SecurityConstants.TOKEN_PREFIX)) 
+            {
 
                 filterChain.doFilter(request, response);
                 return;
+                
             }
+        
             Claims claims = Jwts.parser()
                     .setSigningKey(SecurityConstants.SECRET)
                     .parseClaimsJws(jwt.replace(SecurityConstants.TOKEN_PREFIX, ""))
                     .getBody();
+                    
             String username = claims.getSubject();
-
             ArrayList<Map<String, String>> roles = (ArrayList<Map<String, String>>) claims.get("roles");
             Collection<GrantedAuthority> authorities = new ArrayList<>();
-            roles.forEach(r -> {
+            
+            roles.forEach(r -> 
+            {
                 authorities.add(new SimpleGrantedAuthority(r.get("authority")));
             });
+            
             UsernamePasswordAuthenticationToken authenticatedUser =
                     new UsernamePasswordAuthenticationToken(username, null, authorities);
+                    
             SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
             filterChain.doFilter(request, response);
 
