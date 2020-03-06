6cf4b6e36f312fd94d914260c8c07d4896baf970
Mr.Robot <classic.graal095@gmail.com>
2020-03-06 18:55:51 +0100

    indentation

--- src/main/java/co/simplon/gestiontask/entities/AppUser.java
+++ src/main/java/co/simplon/gestiontask/entities/AppUser.java
@@ -3,48 +3,59 @@ package co.simplon.gestiontask.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
+
 @Entity
-//@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class AppUser {
-    @Id@GeneratedValue
+public class AppUser 
+{
+    
+    @Id
+    @GeneratedValue
     private  Long id;
+    
     @Column(unique = true)
     private String username;
+    
     private String password;
+    
     @ManyToMany(fetch = FetchType.EAGER)
     private Collection<AppRole> roles=new ArrayList<>();
 
-    public Collection<AppRole> getRoles() {
+    public Collection<AppRole> getRoles() 
+    {
         return roles;
     }
 
-    public void setRoles(Collection<AppRole> roles) {
+    public void setRoles(Collection<AppRole> roles) 
+    {
         this.roles = roles;
     }
 
-    public String getUsername() {
+    public String getUsername() 
+    {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsername(String username) 
+    {
         this.username = username;
     }
+    
     @JsonIgnore
     public String getPassword() {
         return password;
     }
+    
     @JsonSetter
-    public void setPassword(String password) {
+    public void setPassword(String password) 
+    {
         this.password = password;
     }
 }
